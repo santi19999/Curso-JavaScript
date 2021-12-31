40cfc18425a3c6658860e0918b2040ec3c2bc8c8
santi19999 <santiherrera1999@gmail.com>
2021-12-31 07:17:55 -0300

    Métodos para Array

---
+++ Map-Filter-Reduce/app.js
@@ -0,0 +1,32 @@
+//const frutas = ['🍌', '🍏', '🍊'];
+//const nuevoArray = frutas.map((fruta) => fruta);//Recorre cada elemento de array anterior y devuelve un array nuevo
+//console.log(nuevoArray);
+//const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//const numerosPorDos = numeros.map((numero) => numero * 2);
+//numerosPorDos.filter()
+//console.log(numerosPorDos);
+
+const users = [
+	{ uid: 1, name: 'Sergio', age: 42, sexo: 'masculino' },
+	{ uid: 2, name: 'Noelia', age: 40, sexo: 'femenino' },
+	{ uid: 3, name: 'Noelia', age: 42, sexo: 'femenino' },
+	{ uid: 4, name: 'Denis', age: 20, sexo: 'femenino' },
+];
+
+//const mayores = users.filter((user) => user.sexo === 'femenino');
+//console.log(mayores);
+/* const amy = users.find((user) => user.name === 'Noelia'); // Devuelve el primer resultado que encuentra ene el array
+console.log(amy); */
+
+/* const existe = users.some((user) => user.name === 'Noelia'); //Devuelve verdadero o falso, si existe al menos un elemento que cumpla la condición
+console.log(existe); */
+
+/* const indice = users.findIndex((user) => user.name === 'Denis'); //devuelve el index o posicion en el arreglo
+console.log(indice);
+ */
+/* const users2 = [
+	{ uid: 6, name: 'santiago', age: 22 },
+	{ uid: 7, name: 'martina', age: 9 },
+];
+const users3 = users.concat(users2); /// Concatena o une dos array en un array nuevo
+console.log(users3); */
