b5dc9afa6f11eff3f6b8f10eb76fb0e74ac03d97
santi19999 <santiherrera1999@gmail.com>
2022-01-04 15:42:56 -0300

    Agregue funcionalidad-quitar total compra

--- DOM/app.js
+++ DOM/app.js
@@ -1,56 +1,91 @@
-/**
- * Primero obtenemos el ul(id=carrito) en cual mostraremos cada elemento que agreguemos al carrito
- * Luego obtenemos el template donde está la lista la cual agregaremos y modificaremos según sea el elemento que se quiera agregar
- * Creamos el Fragment son Nodos del DOM que nunca forman parte del arbol DOM.
- * Obtenemos todos los botones
- */
+/*Primera Parte - Declaración*/
 const carrito = document.getElementById('carrito');
 const template = document.getElementById('template');
 const fragment = document.createDocumentFragment();
-const btnesBotones = document.querySelectorAll('.card .btn');
+const footer = document.getElementById('footer');
+const templateFooter = document.getElementById('templateFooter');
 
-/**
- * Crreamos el carrito, el cual es un objeto literal
- */
-const carritoObjeto = [];
-/**
- * Creamos la función para poder agregar los productos al carrito.
- * Esta recibe como parámetro el evento de cada botón que se clickea.
- * Se crea un objeto con los datos de ese producto seleccionado.
- * Evalúa si existe el producto dentro del carrito, si existe, aumenta la cantidad de unidades y vuelve a mostrar el producto actualizado.
- * Si este no existe, agrega el producto al carrito, luego lo muestra por pantalla.
- */
+/*Segunda Parte -Funciones*/
+document.addEventListener('click', (e) => {
+	if (e.target.matches('.card .btn-outline-primary')) {
+		agregarAlCarrito(e);
+	}
+	if (e.target.matches('div .btn-success')) {
+		aumentarProducto(e);
+	}
+	if (e.target.matches('div .btn-danger')) {
+		disminuirProducto(e);
+	}
+});
+let carritoObjeto = [];
 const agregarAlCarrito = (e) => {
 	const producto = {
 		titulo: e.target.dataset.fruta,
 		id: e.target.dataset.fruta,
 		cantidad: 1,
+		precio: parseInt(e.target.dataset.precio),
 	};
 	const indice = carritoObjeto.findIndex((item) => item.id === producto.id);
 	if (indice === -1) {
 		carritoObjeto.push(producto);
 	} else {
 		carritoObjeto[indice].cantidad++;
+		carritoObjeto[indice].precio =
+			carritoObjeto[indice].cantidad * producto.precio;
 	}
+	console.log(carritoObjeto);
 	mostrarCarrito(carritoObjeto);
 };
-
-/**
- * Esta función es la encargada de recorrer el carrito, clonar el template
- * acceder a los elementos que se modificarán para mostrar el producto
- * modifica y agrega el nombre del producto y la cantidad
- * agrega el clon del template al fragment creado paralelo al DOM
- * luego agrega al ul del DOM La lista modificada del template con el producto que se agrega al carrito
- */
-const mostrarCarrito = (array) => {
+const mostrarCarrito = () => {
 	carrito.textContent = '';
-	array.forEach((item) => {
-		const clone = template.content.firstElementChild.cloneNode(true);
+	carritoObjeto.forEach((item) => {
+		const clone = template.content.cloneNode(true);
 		clone.querySelector('.lead').textContent = item.titulo;
 		clone.querySelector('.badge').textContent = item.cantidad;
+		clone.querySelector('div .lead span').textContent =
+			item.precio * item.cantidad;
+		clone.querySelector('.btn-danger').dataset.id = item.id;
+		clone.querySelector('.btn-success').dataset.id = item.id;
+
 		fragment.appendChild(clone);
 	});
 	carrito.appendChild(fragment);
+	mostrarFooter();
 };
-//Agregamos el evento de clock a cada botón y se dispara la función de agregarAlCarrito
-btnesBotones.forEach((btn) => btn.addEventListener('click', agregarAlCarrito));
+const mostrarFooter = () => {
+	console.log('Mostrar footer');
+	footer.textContent = '';
+	const total = carritoObjeto.reduce(
+		(acc, current) => acc + current.cantidad * current.precio,
+		0
+	);
+	if (total > 0) {
+		const clone = templateFooter.content.cloneNode(true);
+		clone.querySelector('.lead span').textContent = total;
+		footer.appendChild(clone);
+	}
+};
+
+const aumentarProducto = (e) => {
+	carritoObjeto = carritoObjeto.map((item) => {
+		if (item.id === e.target.dataset.id) {
+			item.cantidad++;
+		}
+		return item;
+	});
+	mostrarCarrito();
+};
+const disminuirProducto = (e) => {
+	carritoObjeto = carritoObjeto.filter((item) => {
+		if (item.id === e.target.dataset.id) {
+			if (item.cantidad > 0) {
+				item.cantidad--;
+				if (item.cantidad === 0) return;
+				return item;
+			}
+		} else {
+			return item;
+		}
+	});
+	mostrarCarrito();
+};
