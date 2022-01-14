86dbcc8df3649510e1feeaefa09a6b08d8b4c3e1
santi19999 <santiherrera1999@gmail.com>
2022-01-14 11:50:38 -0300

    no se que paso

--- DOM/app.js
+++ DOM/app.js
@@ -1,40 +1,14 @@
-const cards = document.getElementById('card-dinamicas');
-const templateCard = document.getElementById('template-card').content;
+const templateDivisas = document.getElementById('templateDivisas');
+const divisaActual = document.getElementById('divisaActual');
+const divisaConvertir = document.getElementById('divisaConvertir');
+const btnConvertir = document.getElementById('btnConvertir');
+const inputDivisaActual = document.getElementById('inputDivisaActual');
+const inputDivisaAConvertir = document.getElementById('inputDivisaAConvertir');
+const optionDivisa = document.getElementById('optionDivisa');
+const fragment = document.createDocumentFragment();
 
-document.addEventListener('DOMContentLoaded', () => {
-	fetchData();
-});
+const mostrarDivisas = (divisas) => {};
 
-const fetchData = async () => {
-	try {
-		loadingData(true);
-		const respuesta = await fetch('https://rickandmortyapi.com/api/character');
-		const data = await respuesta.json();
-		mostrarCard(data);
-	} catch (error) {
-		console.log(error);
-	} finally {
-		loadingData(false);
-	}
-};
+const leerDivisas = (divisas) => {};
 
-const mostrarCard = (data) => {
-	const fragment = document.createDocumentFragment();
-	data.results.forEach((item) => {
-		const clone = templateCard.cloneNode(true);
-		clone.querySelector('h5').textContent = item.name;
-		clone.querySelector('img').src = item.image;
-		clone.querySelector('p').textContent = item.species;
-		fragment.appendChild(clone);
-	});
-	cards.appendChild(fragment);
-};
-
-const loadingData = (estado) => {
-	const loading = document.getElementById('loading');
-	if (estado) {
-		loading.classList.remove('d-none');
-	} else {
-		loading.classList.add('d-none');
-	}
-};
+const leerValores = () => {};
